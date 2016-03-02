bf4125893a725bac18b8cb62ef6d608e7e87a758
Kyle Morris <globeofgeek@gmail.com>
2016-03-01 18:45:48 -0600

    Slowly expending Environment interface...

--- core/Environment.h
+++ core/Environment.h
@@ -19,7 +19,22 @@ public:
      * @param string fileName: Name of the environment to load.
      */
     virtual void loadEnvironment(string fileName) = 0;
-};
 
+    /**
+     * Return the state of the environment.
+     * @returns: EnvironmentState - this is a current snapshot of the environment.
+     */
+    virtual EnvironmentState *readState();
+
+    /**
+     * Add a new entity to the environment at a desired location.
+     * Depending on the environment, the Entity and Location may vary. For example
+     * in a 2D grid a Location may consist of (x,y) points, where as in a 3D environment
+     * you would have (x,y,z).
+     * @param Entity *e: The entity you wish to add to the environment.
+     * @param Location *place: The location to add this entity to.
+     */
+    virtual bool add(Entity *e, Location *place);
+};
 
 #endif //MORRIS_AIMA_ENVIRONMENT_H
